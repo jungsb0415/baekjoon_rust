ddfbe7fda629dc4e284c3a6f34c7b2082fa10a76
jungsb0415 <jungsb0415@gmail.com>
2025-07-10 21:05:37 +0900

    solve 33871

---
+++ 33k/33871.rs
@@ -0,0 +1,28 @@
+use std::fmt::Write;
+use std::io::{Read, stdin};
+
+fn main() {
+    let mut input = String::new();
+    stdin().read_to_string(&mut input).unwrap();
+    let mut input: std::str::SplitAsciiWhitespace<'_> = input.split_ascii_whitespace();
+    let mut output = String::new();
+
+    let n = next_u32(&mut input);
+
+    let ans = if n%2 == 1 {
+        "Soomin"
+    } else {
+        "Song"
+    };
+
+    write!(output, "{}\n", ans).unwrap();
+    print!("{output}");
+}
+
+fn next_u32<'a, I: Iterator<Item = &'a str>>(input: &mut I) -> u32 {
+    input.next().unwrap().parse().unwrap()
+}
+
+fn next_i32<'a, I: Iterator<Item = &'a str>>(input: &mut I) -> i32 {
+    input.next().unwrap().parse().unwrap()
+}
