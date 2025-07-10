f20a2820f9ca1d5a3c70ab914d5415fcfd43ea49
jungsb0415 <jungsb0415@gmail.com>
2025-07-10 15:58:28 +0900

    solve 28065

---
+++ 28k/28065.rs
@@ -0,0 +1,31 @@
+use std::fmt::Write;
+use std::io::{Read, stdin};
+
+fn main() {
+    let mut input = String::new();
+    stdin().read_to_string(&mut input).unwrap();
+    let mut input: std::str::SplitAsciiWhitespace<'_> = input.split_ascii_whitespace();
+    let mut output = String::new();
+
+    let n = next_u32(&mut input);
+
+    for i in 0..n {
+        let k = if i%2 == 0 {
+            n - i/2
+        } else {
+            i/2 + 1
+        };
+        write!(output, "{} ", k).unwrap();
+    }
+
+    // write!(output, "{}\n", ans).unwrap();
+    print!("{output}");
+}
+
+fn next_u32<'a, I: Iterator<Item = &'a str>>(input: &mut I) -> u32 {
+    input.next().unwrap().parse().unwrap()
+}
+
+fn next_i32<'a, I: Iterator<Item = &'a str>>(input: &mut I) -> i32 {
+    input.next().unwrap().parse().unwrap()
+}
