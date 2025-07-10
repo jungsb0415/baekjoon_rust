32b372871f71efcf46fb4725bc72b38cddd02bdd
jungsb0415 <jungsb0415@gmail.com>
2025-07-10 15:36:55 +0900

    solve 33365

---
+++ 33k/33365.rs
@@ -0,0 +1,28 @@
+use std::fmt::Write;
+use std::io::{Read, stdin};
+
+fn main() {
+    let mut input = String::new();
+    stdin().read_to_string(&mut input).unwrap();
+    let mut input: std::str::SplitAsciiWhitespace<'_> = input.split_ascii_whitespace();
+    let mut output = String::new();
+
+    let n = next_u32(&mut input);
+
+    let a = if n%2 == 0 {
+        (n-1)/6 * 2 + 2
+    } else {
+        (n-1)/6 * 2 + 1
+    };
+
+    write!(output, "{} {}\n", a, n).unwrap();
+    print!("{output}");
+}
+
+fn next_u32<'a, I: Iterator<Item = &'a str>>(input: &mut I) -> u32 {
+    input.next().unwrap().parse().unwrap()
+}
+
+fn next_i32<'a, I: Iterator<Item = &'a str>>(input: &mut I) -> i32 {
+    input.next().unwrap().parse().unwrap()
+}
