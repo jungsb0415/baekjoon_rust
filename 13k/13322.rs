4c769d17da3ab7821ad6a19fb57015d16a309e2e
jungsb0415 <jungsb0415@gmail.com>
2025-07-12 15:34:56 +0900

    solve 13322

---
+++ 13k/13322.rs
@@ -0,0 +1,28 @@
+use std::fmt::Write;
+use std::io::{Read, stdin};
+
+fn main() {
+    let mut input = String::new();
+    stdin().read_to_string(&mut input).unwrap();
+    let mut input: std::str::SplitAsciiWhitespace<'_> = input.split_ascii_whitespace();
+    let mut output = String::new();
+
+    let name = input.next().unwrap();
+
+    let n = name.len();
+
+    for i in 0..n {
+        write!(output, "{}\n", i).unwrap();
+    }
+
+    // write!(output, "{}\n", ans).unwrap();
+    print!("{output}");
+}
+
+fn next_u32<'a, I: Iterator<Item = &'a str>>(input: &mut I) -> u32 {
+    input.next().unwrap().parse().unwrap()
+}
+
+fn next_i32<'a, I: Iterator<Item = &'a str>>(input: &mut I) -> i32 {
+    input.next().unwrap().parse().unwrap()
+}
