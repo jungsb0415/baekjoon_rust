a960d21177b60e1d997f0946826f67bf7f6f94a9
정승빈 <jungsb0415@gmail.com>
2025-07-18 17:14:02 +0900

    solve 33909

---
+++ 33k/33909.rs
@@ -0,0 +1,30 @@
+use std::fmt::Write;
+use std::io::{Read, stdin};
+
+fn main() {
+    let mut input = String::new();
+    stdin().read_to_string(&mut input).unwrap();
+    let mut input: std::str::SplitAsciiWhitespace<'_> = input.split_ascii_whitespace();
+    let mut output = String::new();
+
+    let s = next_i32(&mut input);
+    let c = next_i32(&mut input);
+    let o = next_i32(&mut input);
+    let n = next_i32(&mut input);
+
+    let c = o*2 + c;
+    let s = s + n;
+
+    let ans = (s / 3).min(c / 6);
+    
+    write!(output, "{}\n", ans).unwrap();
+    print!("{output}");
+}
+
+fn next_u32<'a, I: Iterator<Item = &'a str>>(input: &mut I) -> u32 {
+    input.next().unwrap().parse().unwrap()
+}
+
+fn next_i32<'a, I: Iterator<Item = &'a str>>(input: &mut I) -> i32 {
+    input.next().unwrap().parse().unwrap()
+}
