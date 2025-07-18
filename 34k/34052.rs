0f4fb2ca95567ef4d2cc6af0f4d80297d3363a29
정승빈 <jungsb0415@gmail.com>
2025-07-18 17:10:26 +0900

    solve 34052

---
+++ 34k/34052.rs
@@ -0,0 +1,31 @@
+use std::fmt::Write;
+use std::io::{Read, stdin};
+
+fn main() {
+    let mut input = String::new();
+    stdin().read_to_string(&mut input).unwrap();
+    let mut input: std::str::SplitAsciiWhitespace<'_> = input.split_ascii_whitespace();
+    let mut output = String::new();
+
+    let a = next_i32(&mut input);
+    let b = next_i32(&mut input);
+    let c = next_i32(&mut input);
+    let d = next_i32(&mut input);
+
+    let ans = if a+b+c+d <= 1500 {
+        "Yes"
+    } else {
+        "No"
+    };
+
+    write!(output, "{}\n", ans).unwrap();
+    print!("{output}");
+}
+
+fn next_u32<'a, I: Iterator<Item = &'a str>>(input: &mut I) -> u32 {
+    input.next().unwrap().parse().unwrap()
+}
+
+fn next_i32<'a, I: Iterator<Item = &'a str>>(input: &mut I) -> i32 {
+    input.next().unwrap().parse().unwrap()
+}
