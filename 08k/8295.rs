9e8e0976c4eecb9c17050b3f7a8ecce71c66bd05
정승빈 <jungsb0415@gmail.com>
2025-07-19 02:21:19 +0900

    solve 8295

---
+++ 08k/8295.rs
@@ -0,0 +1,34 @@
+use std::fmt::Write;
+use std::io::{Read, stdin};
+
+fn main() {
+    let mut input = String::new();
+    stdin().read_to_string(&mut input).unwrap();
+    let mut input: std::str::SplitAsciiWhitespace<'_> = input.split_ascii_whitespace();
+    let mut output = String::new();
+
+    let n = next_i32(&mut input);
+    let m = next_i32(&mut input);
+    let p = next_i32(&mut input);
+
+    let mut ans = 0;
+
+    for i in 1..=n {
+        for j in 1..=m {
+            if i+i+j+j >= p {
+                ans += (n-i+1)*(m-j+1);
+            }
+        }
+    }
+
+    write!(output, "{}\n", ans).unwrap();
+    print!("{output}");
+}
+
+fn next_u32<'a, I: Iterator<Item = &'a str>>(input: &mut I) -> u32 {
+    input.next().unwrap().parse().unwrap()
+}
+
+fn next_i32<'a, I: Iterator<Item = &'a str>>(input: &mut I) -> i32 {
+    input.next().unwrap().parse().unwrap()
+}
