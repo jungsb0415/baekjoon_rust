3da42b0d099c1e2a44a12edeb8b6d02e9717603c
정승빈 <jungsb0415@gmail.com>
2025-07-19 02:58:33 +0900

    solve 16515

---
+++ 16k/16515.rs
@@ -0,0 +1,31 @@
+use std::fmt::Write;
+use std::io::{Read, stdin};
+
+fn main() {
+    let mut input = String::new();
+    stdin().read_to_string(&mut input).unwrap();
+    let mut input: std::str::SplitAsciiWhitespace<'_> = input.split_ascii_whitespace();
+    let mut output = String::new();
+
+    let n = next_i32(&mut input);
+
+    let mut ans = 1f64;
+
+    let mut tmp = 1f64;
+
+    for i in 1..=n {
+        tmp /= i as f64;
+        ans += tmp;
+    }
+
+    write!(output, "{}\n", ans).unwrap();
+    print!("{output}");
+}
+
+fn next_u32<'a, I: Iterator<Item = &'a str>>(input: &mut I) -> u32 {
+    input.next().unwrap().parse().unwrap()
+}
+
+fn next_i32<'a, I: Iterator<Item = &'a str>>(input: &mut I) -> i32 {
+    input.next().unwrap().parse().unwrap()
+}
