19b644ed6c3f63cd07e874c5e24bbfc7c1db4114
정승빈 <jungsb0415@gmail.com>
2025-07-19 21:01:06 +0900

    solve 24378

---
+++ 24k/24378.rs
@@ -0,0 +1,32 @@
+use std::fmt::Write;
+use std::io::{Read, stdin};
+
+fn main() {
+    let mut input = String::new();
+    stdin().read_to_string(&mut input).unwrap();
+    let mut input: std::str::SplitAsciiWhitespace<'_> = input.split_ascii_whitespace();
+    let mut output = String::new();
+
+    let flag = next_i32(&mut input);
+    let n = next_i32(&mut input);
+    let a = next_i32(&mut input);
+    let b = next_i32(&mut input);
+    let c = next_i32(&mut input);
+
+    let ans = if flag == 1 {
+        (a+b+c - n*2).max(0)
+    } else {
+        a.min(b.min(c))
+    };
+
+    write!(output, "{}\n", ans).unwrap();
+    print!("{output}");
+}
+
+fn next_u32<'a, I: Iterator<Item = &'a str>>(input: &mut I) -> u32 {
+    input.next().unwrap().parse().unwrap()
+}
+
+fn next_i32<'a, I: Iterator<Item = &'a str>>(input: &mut I) -> i32 {
+    input.next().unwrap().parse().unwrap()
+}
