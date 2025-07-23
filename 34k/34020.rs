e4379075c502dac786738513b9607d58ac3ec02f
정승빈 <jungsb0415@gmail.com>
2025-07-23 16:28:55 +0900

    solve 34020

---
+++ 34k/34020.rs
@@ -0,0 +1,33 @@
+use std::fmt::Write;
+use std::io::{Read, stdin};
+
+fn main() {
+    let mut input = String::new();
+    stdin().read_to_string(&mut input).unwrap();
+    let mut input: std::str::SplitAsciiWhitespace<'_> = input.split_ascii_whitespace();
+    let mut output = String::new();
+
+    let n = next_i32(&mut input);
+
+    let mut v: Vec<i32> = (0..n)
+        .map(|_| next_i32(&mut input)).collect();
+
+    v.sort();
+
+    let mut ans = v[0] as f64;
+
+    for num in v {
+        ans = (ans + num as f64) / 2.0;
+    }
+
+    write!(output, "{}\n", ans).unwrap();
+    print!("{output}");
+}
+
+fn next_u32<'a, I: Iterator<Item = &'a str>>(input: &mut I) -> u32 {
+    input.next().unwrap().parse().unwrap()
+}
+
+fn next_i32<'a, I: Iterator<Item = &'a str>>(input: &mut I) -> i32 {
+    input.next().unwrap().parse().unwrap()
+}
